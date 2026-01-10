8c7af5446fc423f81b030d45910b4cd5a689a1f6
DanAndCastRod <oooglue@utp.edu.co>
2026-01-09 21:53:34 -0500

    fix: downgrade next.js to 15.1.3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
 import type { NextConfig } from "next";
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  // Note: This is only an example.
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
